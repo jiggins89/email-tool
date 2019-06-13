44ef8a8a4226551e5ba4fe612578481242230052
John Higgins <johnhiggins@outlook.ie>
2019-06-13 16:41:56 +0100

    google oauth

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const passport = require("passport");
-const googleStrategy = require("passprt-google-oauth20");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-passport.use(new googleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token: ", accessToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
