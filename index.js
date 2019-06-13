e73e77c9b5add9c496ed351ce90b1ed104b79a4b
John Higgins <johnhiggins@outlook.ie>
2019-06-13 15:09:17 +0100

    added passport js and google oauth

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const passport = require("passport");
+const googleStrategy = require("passprt-google-oauth20");
+
 const app = express();
+
+passport.use(new googleStrategy());
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
