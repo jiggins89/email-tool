abd9447fdb72eebaf954daca28576e3652c63efd
John Higgins <johnhiggins@outlook.ie>
2019-06-14 11:40:58 +0100

    refactored

---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token: ", accessToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
