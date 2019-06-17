22cf3b0d1eddb7d882754c1078aadcbb127d24c4
John Higgins <johnhiggins@outlook.ie>
2019-06-17 13:13:04 +0100

    finsihed authentication

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport"); // needs to be after class imports
@@ -7,6 +9,16 @@ require("./services/passport"); // needs to be after class imports
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,17 +23,20 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      // console.log("this working?", profile.id);
-      User.findOne({ googleId: profile.id })
-        // promise
-        .then(existingUser => {
-          if (existingUser) {
-            // we already have a user
-          } else {
-            // create new user
-            new User({ googleId: profile.id }).save();
-          }
-        });
+      // console.log("this working?", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // null means no issues, then tell who the user is
+          done(null, existingUser);
+        } else {
+          // create new user
+          new User({ googleId: profile.id, email: profile._json.email })
+            // saves new user to the db if they don't exist already
+            .save()
+            // returns promise which is the saved user
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
