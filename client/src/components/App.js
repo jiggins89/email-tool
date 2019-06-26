9d0963051733150c861caef49c78f9147170c912
John Higgins <johnhiggins@outlook.ie>
2019-06-26 09:47:55 +0100

    added routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,26 @@
 // Library imports
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 // Component imports
 
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
